ae0c746f6096974b80e0c7eac364dfb8618d33dd
Tim <timdobranski@gmail.com>
2023-11-20 13:59:58 -0800

    Testing deployment errors: returned feed, but removed Posts render

--- app/page.js
+++ app/page.js
@@ -24,7 +24,7 @@ export default function Home() {
         )}
 
         <ComposePost />
-        {/* <Feed /> */}
+        <Feed />
 
         {showRightSidebar && (
           <div className='sidebarRightWrapper'>
