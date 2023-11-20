44075f0dc25095d24c42306c27a2334cbf57b589
Tim <timdobranski@gmail.com>
2023-11-19 21:01:29 -0800

    Removed sidebars from mobile view

--- app/page.js
+++ app/page.js
@@ -1,21 +1,37 @@
+'use client';
+
 import Image from 'next/image'
 import ComposePost from './components/ComposePost/ComposePost';
 import SidebarLeft from './components/SidebarLeft/SidebarLeft';
 import SidebarRight from './components/SidebarRight/SidebarRight';
 import Feed from './components/Feed/Feed';
+import { useState } from 'react';
 
 export default function Home() {
+  // States to manage the visibility of sidebars
+  const [showLeftSidebar, setShowLeftSidebar] = useState(true);
+  const [showRightSidebar, setShowRightSidebar] = useState(true);
+
   return (
     <>
-      <div className='appContent'>
-      <h1 className='title'>Canine Corners</h1>
-      <SidebarLeft />
+      <div className={'appContent'}>
+        <h1 className='title'>Canine Corners</h1>
+
+        {showLeftSidebar && (
+          <div className='sidebarLeftWrapper'>
+            <SidebarLeft />
+          </div>
+        )}
+
         <ComposePost />
         <Feed />
-      <SidebarRight />
+
+        {showRightSidebar && (
+          <div className='sidebarRightWrapper'>
+            <SidebarRight />
+          </div>
+        )}
       </div>
     </>
-
-
-  )
+  );
 }
